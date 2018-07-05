26c3c59a080934e4597534611568dd503ac0a399
MoLeighJackson <moleighjackson@gmail.com>
2018-07-05 12:45:12 -0400

    Changed string resource for app name from miwok to k'iche'

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
 
                 // Start the new activity
-                startActivity(colorsIntent);
+              startActivity(colorsIntent);
             }
         });
 
